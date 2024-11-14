587f87b1f4c172766258dc0ee5e09eeaed6509ba
KursatW <kursat.kilitci19@gmail.com>
2024-11-14 18:01:13 +0300

    CrudV1.0

---
+++ src/main/java/com/codecraft/Crud_app/service/TaskService.java
@@ -0,0 +1,33 @@
+package com.codecraft.Crud_app.service;
+
+import com.codecraft.Crud_app.model.Task;
+import com.codecraft.Crud_app.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public List<Task> getAllEmployees() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTaskById(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+}
